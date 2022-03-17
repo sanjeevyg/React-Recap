20cb8cf9586045bf3d17c1a21b81fec735a476e5
sanjeevyg <yogi.sjv@gmail.com>
2022-03-17 15:39:38 -0400

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from "@testing-library/react";
+import React from "react";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("h1 content", () => {
+  const { getByText } = render(<App />);
+  const h1 = getByText('This is react!');
+  expect(h1).toHaveTextContent('This is react!')
+})
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App/>,
+  <App login={"sanjeevyg"}/>,
   document.getElementById('root')
 );
 
